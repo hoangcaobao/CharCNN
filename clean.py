8262f87ed0a1642b9a2bc88f270a6155e120e087
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-08-10 11:18:28 +0700

    Update clean.py

--- clean.py
+++ clean.py
@@ -10,7 +10,7 @@ class CleanData:
         self.test_size=test_size
     
     def find_max_len(self):
-        self.len_sentences=[len(i) for i in self.sentences]
+        self.len_sentences=[len(i) for i in self.x_train]
         self.max_len=max(self.len_sentences)
     
     def split_data(self):
@@ -21,17 +21,20 @@ class CleanData:
         self.token=Tokenizer(self.vocab_size, oov_token="<OOV>")
         self.token.fit_on_texts(self.x_train)
 
-    def pad_data(self):
+    def change_to_sequence(self):
         self.x_train=self.token.texts_to_sequences(self.x_train)
         self.x_valid=self.token.texts_to_sequences(self.x_valid)
+        
+    def pad_data(self):
         self.x_train=pad_sequences(self.x_train, maxlen=self.max_len, padding="post")
         self.x_valid=pad_sequences(self.x_valid, maxlen=self.max_len, padding="post")
         self.x_train=np.array(self.x_train)
         self.x_valid=np.array(self.x_valid)
 
     def preprocessing_data(self):
-        self.find_max_len()
         self.split_data()
         self.tokenize()
+        self.change_to_sequence()
+        self.find_max_len()
         self.pad_data()
        
